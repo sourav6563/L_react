67be33d92fa3cc3227d5af2ddb4eca49800cf175
sourav <souravmahato6563@gmail.com>
2025-09-12 21:31:25 +0530

    feat: add color buttons to change background color in App component

--- 04bgChanger/src/App.jsx
+++ 04bgChanger/src/App.jsx
@@ -10,8 +10,56 @@ function App() {
       style={{ backgroundColor: color }}
     >
       <div className="fixed flex flex-wrap justify-center bottom-12 inset-x-0 px-2">
-        <div className="flex flex-wrap justify-center gap-3 shadow-lg bg-red-900 text-white">
-          test
+        <div className="flex flex-wrap justify-center gap-3 shadow-lg bg-white  px-3 py-2 rounded-3xl">
+          <button
+            onClick={() => setColor("red")}
+            className=" outline-none px-4 py-1 rounded-full text-white"
+            style={{ backgroundColor: "#82181a" }}
+          >
+            Red
+          </button>{" "}
+          <button
+            onClick={() => setColor("green")}
+            className=" outline-none px-4 py-1 rounded-full text-white"
+            style={{ backgroundColor: "green" }}
+          >
+            green
+          </button>{" "}
+          <button
+            onClick={() => setColor("purple")}
+            className=" outline-none px-4 py-1 rounded-full text-white"
+            style={{ backgroundColor: "purple" }}
+          >
+            purple
+          </button>{" "}
+          <button
+            onClick={() => setColor("blue")}
+            className=" outline-none px-4 py-1 rounded-full text-white"
+            style={{ backgroundColor: "blue" }}
+          >
+            blue
+          </button>{" "}
+          <button
+            onClick={() => setColor("pink")}
+            className=" outline-none px-4 py-1 rounded-full text-white"
+            style={{ backgroundColor: "pink" }}
+          >
+            pink
+          </button>
+          <button
+            onClick={() => setColor("olive")}
+            className=" outline-none px-4 py-1 rounded-full text-white"
+            style={{ backgroundColor: "olive" }}
+          >
+            olive
+          </button>
+          <button
+            onClick={() => setColor("black")}
+            className=" outline-none px-4 py-1 rounded-full text-white"
+            style={{ backgroundColor: "black" }}
+          >
+            black
+          </button>
         </div>
       </div>
     </div>
