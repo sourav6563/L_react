ac9f57f4321655e6e813da07ca9d94aa92358106
sourav <souravmahato6563@gmail.com>
2025-10-07 20:35:10 +0530

    feat: refactor App component and integrate Tailwind CSS; add useCurrencyInfo hook for currency data fetching

---
+++ 06currencyConvertor/src/hooks/useCurrencyInfo.js
@@ -0,0 +1,16 @@
+import { useEffect, useState } from "react";
+
+const useCurrencyInfo = (currency) => {
+  const [data, setData] = useState();
+  const API = `https://cdn.jsdelivr.net/npm/@fawazahmed0/currency-api@latest/v1/currencies/${currency}.json`;
+  useEffect(() => {
+    fetch(API)
+      .then((res) => res.json())
+      .then((res) => setData(res[currency]));
+    console.table(data);
+  }, [currency]);
+
+  return data;
+};
+
+export { useCurrencyInfo };
